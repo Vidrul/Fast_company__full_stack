9dfb00c244ec7fac21acdff03c7e7b9e126ce6b5
David Saley <davidka907@gmail.com>
2022-03-09 14:31:05 +0300

    fix

--- Server/app.js
+++ Server/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use("/api", routes);
 
-const PORT = config.get("port") ?? 8080;
+const PORT = process.env.PORT || 8080;
 
 if (process.env.NODE_ENV === "production") {
   console.log("Production");
